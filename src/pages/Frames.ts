7e6149bf3567ecfa9098695151a6228923fbb9e6
Hamza Kundi <hamza.kkundi@gmail.com>
2025-05-04 18:08:28 -0700

    added frames constants and images

---
+++ src/pages/Frames.ts
@@ -0,0 +1,52 @@
+// offset corresponds to the percentage of ear-distance
+const imageDir = '${imageDir}';
+export const FRAMES: { name: string; image: string; scale: number; offset: number }[] = [
+  {
+    name: 'Wayfarer',
+    image: '${imageDir}wayfarer.png',
+    scale: 1.1,
+    offset: 0.02,
+  },
+  {
+    name: 'Aviator',
+    image: '${imageDir}aviator.png',
+    scale: 1.2,
+    offset: 0.03,
+  },
+  {
+    name: 'Cat-Eye',
+    image: '${imageDir}cat_eye.png',
+    scale: 1.15,
+    offset: 0.025,
+  },
+  {
+    name: 'Circle',
+    image: '${imageDir}circle.png',
+    scale: 1.0,
+    offset: 0.015,
+  },
+  {
+    name: 'Rectangle',
+    image: '${imageDir}rectangle.png',
+    scale: 1.0,
+    offset: 0.015,
+  },
+  {
+    name: 'Square',
+    image: '${imageDir}square.png',
+    scale: 1.1,
+    offset: 0.02,
+  },
+  {
+    name: 'Oval',
+    image: '${imageDir}oval.png',
+    scale: 1.05,
+    offset: 0.015,
+  },
+  {
+    name: 'Oversized',
+    image: '${imageDir}oversized.png',
+    scale: 1.0,
+    offset: 0.01,
+  },
+];
